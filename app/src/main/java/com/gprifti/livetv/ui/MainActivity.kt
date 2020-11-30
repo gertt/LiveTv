555fa9bb0fd05a8699e4a6b9f725d9897e574e39
elioprifti@gmail.com <gertiprifti@gmail.com>
2020-11-30 20:02:07 +0100

    add dimens

--- app/src/main/java/com/gprifti/livetv/ui/MainActivity.kt
+++ app/src/main/java/com/gprifti/livetv/ui/MainActivity.kt
@@ -2,11 +2,15 @@ package com.gprifti.livetv.ui
 
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import com.amitshekhar.DebugDB
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.gprifti.livetv.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,35 +19,29 @@ class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
-        //   navController = findNavController(this, R.id.fragment)
-        //  bottomNav.setupWithNavController(navController)
 
-        var sgs = DebugDB.getAddressLog();
-        // Open http://192.168.1.2:8080 in your browser
-
-        //setupNav()
+        setupNav()
     }
 
-//    private fun setupNav() {
-//        val navController = findNavController(R.id.fragment)
-//        findViewById<BottomNavigationView>(R.id.bottomNav).setupWithNavController(navController)
-//        navController.addOnDestinationChangedListener { _, destination, _ ->
-//            when (destination.id) {
-//                R.id.favoriteFragment -> showBottomNav()
-//                R.id.popularListFragment -> showBottomNav()
-//                R.id.searchFragment -> showBottomNav()
-//                else -> hideBottomNav()
-//            }
-//        }
-//    }
+    private fun setupNav() {
+        val navController = findNavController(R.id.fragment)
+        findViewById<BottomNavigationView>(R.id.bottomNav).setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.favoriteFragment -> showBottomNav()
+                R.id.popularListFragment -> showBottomNav()
+                R.id.searchFragment -> showBottomNav()
+                else -> hideBottomNav()
+            }
+        }
+    }
 
-//    private fun showBottomNav() {
-//        bottomNav.visibility = View.VISIBLE
-//    }
-//
-//    private fun hideBottomNav() {
-//        bottomNav.visibility = View.GONE
-//    }
+    private fun showBottomNav() {
+        bottomNav.visibility = View.VISIBLE
+    }
+
+    private fun hideBottomNav() {
+        bottomNav.visibility = View.GONE
+    }
 }
